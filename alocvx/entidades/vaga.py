519a3b384979c3fb28a71c862c342dc71be66dc7
Moacir Junior <skytec.dev@gmail.com>
2026-01-20 22:08:11 -0300

    Adicionado a classe vaga

---
+++ alocvx/entidades/vaga.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import List, ClassVar
+
+
+@dataclass
+class Vaga:
+    numero: int
+    localizacao: tuple
+    vagas: ClassVar[List["Vaga"]] = []
+
+    @staticmethod
+    def obter_vaga_pelo_numero(numero: int) -> dict:
+        for vaga in Vaga.vagas:
+            if vaga.numero == numero:
+                return {
+                    "ok": True,
+                    "message": "Vaga encontrada",
+                    "data": {"vaga": vaga},
+                }
+        return {"ok": False, "message": "Vaga não encontrada"}
+
+    @staticmethod
+    def obter_vaga_pela_localizacao(localizacao: tuple) -> dict:
+        for vaga in Vaga.vagas:
+            if vaga.localizacao == localizacao:
+                return {
+                    "ok": True,
+                    "message": "Vaga encontrada",
+                    "data": {"vaga": vaga},
+                }
+        return {"ok": False, "message": "Vaga não encontrada"}
+
+    @staticmethod
+    def calcular_distancia(vaga1: "Vaga", vaga2: "Vaga") -> dict:
+        return {
+            "ok": True,
+            "message": "Distância calculada",
+            "data": {
+                "distancia": abs(vaga1.localizacao[0] - vaga2.localizacao[0])
+                + abs(vaga1.localizacao[1] - vaga2.localizacao[1])
+            },
+        }
