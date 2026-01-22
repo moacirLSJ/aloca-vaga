7321c2c4649c47e7f0393dc225747cabfe8378bf
Moacir <skytec.dev@gmail.com>
2026-01-22 15:03:31 -0300

    funcionalidade: adicionado a classe para processar a alocao pelo vizinho mais distante.

---
+++ alocvx/engrenagems/vizinho_mais_distante.py
@@ -0,0 +1,50 @@
+from alocvx.entidades.morador import Morador
+from alocvx.utilidades.progressbar import progress_bar
+from alocvx.engrenagems.troca_vaga_contrato import TrocaVagaEngrenagem
+
+class VizinhoMaisDistanteEngrenagem(TrocaVagaEngrenagem):
+    def __init__(self, moradores: list["Morador"]):
+        self.moradores = moradores
+
+    def otimizar_alocacao(self) -> list["Morador"]:
+        # implementar a lógica de troca de vagas entre moradores
+        # para minimizar a soma total dos passos
+        # estratégia simples: tentar trocar vagas entre o morador mais distante e os outros
+        melhor_moradores = self.moradores[:]
+        melhor_passos = Morador.calcular_passos_moradores(melhor_moradores)
+        melhor_encontrado = True
+    
+        while melhor_encontrado:
+            melhor_encontrado = False
+            # encontrar o morador com o maior número de passos
+            morador_mais_distante = max(
+                melhor_moradores, key=lambda m: m.calcular_distancia_passos()['data']
+            )
+
+            for morador in melhor_moradores:
+                progress_bar(
+                    morador_mais_distante.apt * len(melhor_moradores) + morador.apt,
+                    len(melhor_moradores) * len(melhor_moradores),
+                )
+                if morador == morador_mais_distante:
+                    continue
+
+                # trocar as vagas alocadas entre os dois moradores
+                morador_mais_distante.vaga_alocada, morador.vaga_alocada = (
+                    morador.vaga_alocada,
+                    morador_mais_distante.vaga_alocada,
+                )
+
+                passos_atuais = Morador.calcular_passos_moradores(melhor_moradores)
+
+                if passos_atuais < melhor_passos:
+                    melhor_passos = passos_atuais
+                    melhor_encontrado = True
+                else:
+                    # desfazer a troca se não melhorar
+                    morador_mais_distante.vaga_alocada, morador.vaga_alocada = (
+                        morador.vaga_alocada,
+                        morador_mais_distante.vaga_alocada,
+                    )
+
+        return melhor_moradores
